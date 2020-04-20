94752a863d132fce7aa4b3d35a6270b94d6d175c
Okke Formsma <okke@formsma.nl>
2020-04-20 11:44:45 +0200

    cleanup of okke user keymap

--- users/okke/rules.mk
+++
@@ -1,5 +0,0 @@
-# generated from users/manna-harbour_miryoku/miryoku.org  -*- buffer-read-only: t -*-
-
-MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
-EXTRAFLAGS += -flto	     # Link Time Optimization to reduce code size, 31358->28034/28672
