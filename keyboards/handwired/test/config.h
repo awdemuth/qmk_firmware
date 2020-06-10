c684dfd22079ae17da47e0acd87001404b99df66
Okke Formsma <okke@formsma.nl>
2020-06-10 22:01:52 +0200

    ps2 trackpoint working with interrupts on pd2 and pd3

--- keyboards/handwired/test/config.h
+++ keyboards/handwired/test/config.h
@@ -47,10 +47,10 @@
 #    define PS2_CLOCK_PIN PIND
 #    define PS2_CLOCK_DDR DDRD
 #    define PS2_CLOCK_BIT 3
-#    define PS2_DATA_PORT PORTC
-#    define PS2_DATA_PIN PINC
-#    define PS2_DATA_DDR DDRC
-#    define PS2_DATA_BIT 6
+#    define PS2_DATA_PORT PORTD
+#    define PS2_DATA_PIN PIND
+#    define PS2_DATA_DDR DDRD
+#    define PS2_DATA_BIT 2
 #endif
 
 
@@ -59,14 +59,14 @@
 #define PS2_CLOCK_PIN   PIND
 #define PS2_CLOCK_DDR   DDRD
 #define PS2_CLOCK_BIT   3
-#define PS2_DATA_PORT   PORTC
-#define PS2_DATA_PIN    PINC
-#define PS2_DATA_DDR    DDRC
-#define PS2_DATA_BIT    6
+#define PS2_DATA_PORT   PORTD
+#define PS2_DATA_PIN    PIND
+#define PS2_DATA_DDR    DDRD
+#define PS2_DATA_BIT    2
 
 #define PS2_INT_INIT()  do {    \
     EICRA |= ((1<<ISC30) |      \
-              (0<<ISC21));      \
+              (0<<ISC31));      \
 } while (0)
 #define PS2_INT_ON()  do {      \
     EIMSK |= (1<<INT3);         \
@@ -76,29 +76,3 @@
 } while (0)
 #define PS2_INT_VECT   INT3_vect
 #endif
-/*
-#ifdef PS2_USE_INT
-#    define PS2_CLOCK_PORT PORTB
-#    define PS2_CLOCK_PIN PINB
-#    define PS2_CLOCK_DDR DDRB
-#    define PS2_CLOCK_BIT 5
-#    define PS2_DATA_PORT PORTB
-#    define PS2_DATA_PIN PINB
-#    define PS2_DATA_DDR DDRB
-#    define PS2_DATA_BIT 4
-
-#    define PS2_INT_INIT()                          \
-        do {                                        \
-            EICRA |= ((1 << ISC21) | (0 << ISC20)); \
-        } while (0)
-#    define PS2_INT_ON()          \
-        do {                      \
-            EIMSK |= (1 << INT2); \
-        } while (0)
-#    define PS2_INT_OFF()          \
-        do {                       \
-            EIMSK &= ~(1 << INT2); \
-        } while (0)
-#    define PS2_INT_VECT INT2_vect
-#endif
-*/
